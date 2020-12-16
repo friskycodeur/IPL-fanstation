17e61a2a39d601daa5203dbce5ad8f82df51c82c
Prateek Maheshwari <friskycodeur@gmail.com>
2020-12-17 01:29:50 +0530

    feat:User (django-app)

--- IPLfanstation/settings.py
+++ IPLfanstation/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'players.apps.PlayersConfig',
     'crispy_forms',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
+CRISPY_TEMPLATE_PACK= 'bootstrap4'
+
+LOGIN_REDIRECT_URL='blog-home'
+LOGIN_URL='login'
